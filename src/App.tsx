fdfc6b51383ce4a4180afb8576ea4ecf6ef1a304
kobiyoyo <adamachubi@gmail.com>
2021-11-14 00:08:08 +0100

    feat(mapview.tsx): add map to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import MapView from './components/MapView/index';
 
 const App = function () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/mapview" element={<MapView />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/components/TreeView/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TreeView = () => {
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default TreeView;
